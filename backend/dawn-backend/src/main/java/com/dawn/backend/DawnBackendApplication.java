ca8b4da216ff68f49a409cf43f7ef1888379c89e
송준형 <ssong99999@naver.com>
2025-01-22 21:22:57 +0900

    ✨ feat : init

---
+++ backend/dawn-backend/src/main/java/com/dawn/backend/DawnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dawn.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawnBackendApplication.class, args);
+	}
+
+}
