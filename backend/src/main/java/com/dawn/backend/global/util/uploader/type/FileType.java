102ed1d4330600e333f0fb253407e4545fe9f14d
송준형 <ssong99999@naver.com>
2025-02-10 17:23:12 +0900

    🐛 fix : fix filetype
    (cherry picked from commit 6fc0d1c572343ac735a727225e5f16d026b5613b)

--- backend/src/main/java/com/dawn/backend/global/util/uploader/type/FileType.java
+++ backend/src/main/java/com/dawn/backend/global/util/uploader/type/FileType.java
@@ -1,8 +1,28 @@
 package com.dawn.backend.global.util.uploader.type;
 
+import lombok.Getter;
+
+@Getter
 public enum FileType {
-	JPG,
-	PNG,
-	DWG,
-	PDF
+	JPG("image/jpg"),
+	JPEG("image/jpeg"),
+	PNG("image/png"),
+	DWG("application/acad"),
+	PDF("application/pdf");
+
+	private final String type;
+
+	FileType(String type) {
+		this.type = type;
+	}
+
+	public static FileType getFileType(String type) {
+		for (FileType ft : FileType.values()) {
+			if (ft.getType().equals(type)) {
+				return ft;
+			}
+		}
+
+		return null;
+	}
 }
