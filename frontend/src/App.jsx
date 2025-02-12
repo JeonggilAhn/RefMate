8766462321d56322f6db8972f19c5b4d7bc248cf
이승연 <ciociolee97@gmail.com>
2025-02-12 13:32:01 +0900

    Resolve S12P11A807-353 "Feature fe/ router error"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import Form from './components/common/Form';
 import Confirm from './components/common/Confirm';
