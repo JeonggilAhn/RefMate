0c8d81622999566fbcad72f333d7d73ec71947f1
송준형 <ssong99999@naver.com>
2025-02-12 15:23:53 +0900

    👺 hotfix : romove hash to change browser router
    (cherry picked from commit d4668ef3c2cfd32e818adc915e7ec7f3e67f1942)

--- backend/src/main/java/com/dawn/backend/global/util/email/service/EmailService.java
+++ backend/src/main/java/com/dawn/backend/global/util/email/service/EmailService.java
@@ -41,7 +41,7 @@ public class EmailService {
 
 			// login 페이지 url 확정 시 추가예정
 			String inviteLink = "?grant_token=" + grantToken;
-			String unauthorizedInviteLink = frontUrl + "/#/projects/" + projectId + "/blueprints"
+			String unauthorizedInviteLink = frontUrl + "/projects/" + projectId + "/blueprints"
 				+ "?presigned=" + unauthorizedGrantToken;
 			content = content
 				.replace("{projectTitle}", projectTitle)
