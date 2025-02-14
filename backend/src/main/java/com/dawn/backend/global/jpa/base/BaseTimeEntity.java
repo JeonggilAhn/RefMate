5f8dee133e07c18b96219c72fd225e27cf4379a4
송준형 <ssong99999@naver.com>
2025-02-14 14:34:52 +0900

    🐛 fix : set db time to seoul

--- backend/src/main/java/com/dawn/backend/global/jpa/base/BaseTimeEntity.java
+++ backend/src/main/java/com/dawn/backend/global/jpa/base/BaseTimeEntity.java
@@ -1,18 +1,29 @@
 package com.dawn.backend.global.jpa.base;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity extends BaseCreatedTimeEntity {
+	private static final ZoneId seoul = ZoneId.of("Asia/Seoul");
+
 	@LastModifiedDate
 	private LocalDateTime updatedAt;
+
+	@PrePersist
+	public void prePersist() {
+		if (updatedAt == null) {
+			updatedAt = LocalDateTime.now(seoul);
+		}
+	}
 }
