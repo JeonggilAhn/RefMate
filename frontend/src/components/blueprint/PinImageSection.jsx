8e4585d6b3e30080b9881e8bd2fdf4a581698368
김햇빛 <kmn3626@naver.com>
2025-02-17 16:38:51 +0900

    ⚙️ chore : modify pin img section S12P11A807-492

--- frontend/src/components/blueprint/PinImageSection.jsx
+++ frontend/src/components/blueprint/PinImageSection.jsx
@@ -28,42 +28,48 @@ const PinImageSection = ({ pinId, onClickImage }) => {
         레퍼런스
       </div>
       <div ref={bottomRef} className="h-50 overflow-y-auto p-2">
-        {data.pinDetailImages.map((pin) => {
-          const images = pin.image_list.slice(0, 3); // 최대 3개 표시
-          return (
-            <div key={pin.note_id} className="mb-2">
-              <div className="text-sm font-medium bg-gray-100 px-2 py-1 rounded-md">
-                {pin.note_title}
+        {data.pinDetailImages.length === 0 ? (
+          <div className="text-sm text-left text-gray-500 p-4">
+            등록된 레퍼런스가 없습니다.
+          </div>
+        ) : (
+          data.pinDetailImages.map((pin) => {
+            const images = pin.image_list.slice(0, 3); // 최대 3개 표시
+            return (
+              <div key={pin.note_id} className="mb-2">
+                <div className="text-sm font-medium bg-gray-100 px-2 py-1 rounded-md">
+                  {pin.note_title}
+                </div>
+                <div className="grid gap-1 mt-2 grid-cols-3 place-items-center">
+                  {images.map((item, idx) => (
+                    <div
+                      key={item.image_id}
+                      className="relative w-[6.1rem] h-[6.1rem]"
+                      onClick={() => onClickImage(pin.image_list, idx)}
+                    >
+                      <img
+                        src={item.image_preview}
+                        alt="reference"
+                        className="w-full h-full object-cover rounded-md"
+                      />
+                      {item.is_bookmark && (
+                        <div
+                          className="absolute top-0 right-0 w-4 h-4 clip-triangle"
+                          style={{ backgroundColor: '#87b5fa' }}
+                        ></div>
+                      )}
+                      {idx === 2 && pin.image_list.length > 3 && (
+                        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-sm font-semibold rounded-md">
+                          +{pin.preview_image_list.length - 3}
+                        </div>
+                      )}
+                    </div>
+                  ))}
+                </div>
               </div>
-              <div className="grid gap-1 mt-2 grid-cols-3 place-items-center">
-                {images.map((item, idx) => (
-                  <div
-                    key={item.image_id}
-                    className="relative w-[6.1rem] h-[6.1rem]"
-                    onClick={() => onClickImage(pin.image_list, idx)}
-                  >
-                    <img
-                      src={item.image_preview}
-                      alt="reference"
-                      className="w-full h-full object-cover rounded-md"
-                    />
-                    {item.is_bookmark && (
-                      <div
-                        className="absolute top-0 right-0 w-4 h-4 clip-triangle"
-                        style={{ backgroundColor: '#87b5fa' }}
-                      ></div>
-                    )}
-                    {idx === 2 && pin.image_list.length > 3 && (
-                      <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-sm font-semibold rounded-md">
-                        +{pin.preview_image_list.length - 3}
-                      </div>
-                    )}
-                  </div>
-                ))}
-              </div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
     </div>
   );
